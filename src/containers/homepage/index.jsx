8a73ab6177935dd20d02a0bbee32d1808c73bfd1
aindriu80 <aindriu80@gmail.com>
2021-02-26 21:37:44 +0000

    Top section image

--- src/containers/homepage/index.jsx
+++ src/containers/homepage/index.jsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
 import React from "react";
+import { TopSection } from "./topSection";
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+`;
 
 export function Homepage(props) {
-  return <div>Hello World</div>;
+  return (
+    <PageContainer>
+      <TopSection />
+    </PageContainer>
+  );
 }
---
+++ src/containers/homepage/topSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+import BackgroundImg from "../../assets/pictures/company_team.jpg";
+
+const TopContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  background-image: url(${BackgroundImg});
+`;
+
+export function TopSection(props) {
+  return <TopContainer>booom</TopContainer>;
+}
