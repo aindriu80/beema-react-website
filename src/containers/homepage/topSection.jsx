791273f004f53ee48604896cc932dd0ab599307d
aindriu80 <aindriu80@gmail.com>
2021-02-27 22:04:03 +0000

    dark cover

--- src/containers/homepage/topSection.jsx
+++ src/containers/homepage/topSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import BackgroundImg from "../../assets/pictures/company_team.jpg";
+import { Logo } from "../../components/logo";
 
 const TopContainer = styled.div`
   width: 100%;
@@ -10,6 +11,21 @@ const TopContainer = styled.div`
   background-image: url(${BackgroundImg});
 `;
 
+const BackgroundFilter = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(53, 53, 54, 0.88);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export function TopSection(props) {
-  return <TopContainer>booom</TopContainer>;
+  return (
+    <TopContainer>
+      <BackgroundFilter>
+        <Logo />
+      </BackgroundFilter>
+    </TopContainer>
+  );
 }
