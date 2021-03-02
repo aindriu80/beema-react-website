d100e072071b70eebd405ea466964dff3be0e4c1
aindriu80 <aindriu80@gmail.com>
2021-03-02 15:34:44 +0000

    Top Nav Section tidy

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Button } from "../button/button";
 import { Logo } from "../../components/logo/logo";
 
+import { Marginer } from "../../components/marginer/marginer";
+
 const NavbarContainer = styled.div`
   width: 100%;
   height: 50px;
@@ -14,13 +16,17 @@ const NavbarContainer = styled.div`
 
 const BrandContainer = styled.div``;
 
-const AccessibilityContainer = styled.div``;
+const AccessibilityContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const LoginButton = styled(Button)`
   background-color: transparent;
   border: none;
   &:hover {
     background-color: transparent;
+    border: none;
     color: rgb(200, 200, 200);
   }
 `;
@@ -33,6 +39,8 @@ export function Navbar(props) {
       </BrandContainer>
       <AccessibilityContainer>
         <Button small>Get Started</Button>
+        <Marginer direction="horizontal" margin="8px" />
+        <LoginButton small>Login</LoginButton>
       </AccessibilityContainer>
     </NavbarContainer>
   );
